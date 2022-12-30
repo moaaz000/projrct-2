1e7db43305b16c43e711da61e45c160f6b5e3330
moaaz000 <121647204+moaaz000@users.noreply.github.com>
2022-12-30 14:49:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+
+int n1, n2;
+Console.WriteLine("Please enter frst number ");
+n1= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("please enter second number");
+n2= Convert.ToInt32(Console.ReadLine());
+for(int i =n1; i<=n2; i++)
+{
+    if(isperfect(i))
+    {
+        Console.WriteLine("i");
+    }
+}
+
+static bool isperfect(int n)
+{
+    int sum = 0;
+    for (int i = 1; i < n;i++)
+    { 
+
+        if (n % i == 0)
+        {
+            sum+= i;
+       
+        }
+    }
+    return sum == n;
+
+}
